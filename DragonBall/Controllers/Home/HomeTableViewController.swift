9cca6e5647e12c89f0c163fec6eef51ff83ae24a
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-09-20 23:01:04 +0200

    Add List Cell

--- DragonBall/Controllers/Home/HomeTableViewController.swift
+++ DragonBall/Controllers/Home/HomeTableViewController.swift
@@ -1,11 +1,52 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    
+    private let network = NetworkModel()
+    
+    var heroes: [Hero] = []
         
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Heroes"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        network.getHeroes(completion: { [weak self] result in
+            guard case let .success(hero) = result else {
+                return
+            }
+                          
+            self?.heroes = hero
+        })
     }
     
+}
+
+// MARK: - Data Source
+extension HomeTableViewController {
+    // Set Size
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        heroes.count
+    }
+    
+    // Catch cell and add object
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: HomeTableViewCell.identifier,
+            for: indexPath
+        ) as? HomeTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let hero = heroes[indexPath.row]
+        cell.configure(with: hero)
+        return cell
+    }
 }
---
+++ DragonBall/Views/HomeTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    static let identifier = "HouseTableViewCell"
+    
+    func configure(with hero: Hero) {
+    }
+    
+}
